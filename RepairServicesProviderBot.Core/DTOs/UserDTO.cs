5f4479e362b131f2d0f123c5361312de20959b72
Nikolay <tentotennot@mail.ru>
2024-08-17 22:17:26 +0300

    UserDTO

--- RepairServicesProviderBot.Core/DTOs/UserDTO.cs
+++ RepairServicesProviderBot.Core/DTOs/UserDTO.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace RepairServicesProviderBot.Core.DTOs
 {
-    internal class UserDTO
+    public class UserDTO
     {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public string? Image { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        //public List<Contactor_ServiceType>? Contactor_ServiceType { get; set; }
     }
 }
