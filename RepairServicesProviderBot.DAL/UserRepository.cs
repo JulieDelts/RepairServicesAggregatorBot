2eb086ca078c67933e01294c9b9bf4f7f9a8e85d
NikiDumb <tentotennot@mail>
2024-08-17 22:35:07 +0300

    Create query and func for add user

--- RepairServicesProviderBot.DAL/UserRepository.cs
+++ RepairServicesProviderBot.DAL/UserRepository.cs
@@ -1,7 +1,31 @@
-﻿namespace RepairServicesProviderBot.DAL
+﻿using Npgsql;
+using Dapper;
+using RepairServicesProviderBot.Core;
+using RepairServicesProviderBot.Core.DTOs;
+using RepairServicesProviderBot.DAL.Querries;
+
+namespace RepairServicesProviderBot.DAL
 {
     public class UserRepository
     {
+        public void AddUser(UserDTO user)
+        {
+            string conectionString = Options.ConnectionString;
+            using (var connection = new NpgsqlConnection(conectionString))
+            {
+                string query = Querries.UserQuerys.AddUserQuery;
+                var args = new { id = user.Id, 
+                    name = user.Name,
+                    email = user.Email,
+                    phone = user.Phone,
+                    roleId = user.RoleId,
+                    image = user.Image,
+                    isDeleted = user.IsDeleted
+                };
 
+                connection.Open();
+                connection.Query(query, args);
+            }
+        }
     }
 }
