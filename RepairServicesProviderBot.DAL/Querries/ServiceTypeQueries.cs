9a3e6bb02862e7755b3d6f3eefc40fb0e18955b2
JulieDelts <juliadelcova@gmail.com>
2024-08-18 01:30:15 +0300

    Add ServiceTypeQueries and ServiceTypeRepository

---
+++ RepairServicesProviderBot.DAL/Querries/ServiceTypeQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairServicesProviderBot.DAL.Querries
+{
+    public class ServiceTypeQueries
+    {
+        public const string AddServiceType = "SELECT * FROM \"AddServiceType\"(@description);";
+
+        public const string HideServiceTypeById = "SELECT * FROM \"HideServiceTypeById\"(@serviceTypeId);";
+
+        public const string GetAllServiceTypes = "SELECT * FROM \"GetAllServiceTypes\"(@serviceTypeId);";
+
+        public const string UpdateServiceTypeById = "SELECT * FROM \"UpdateServiceTypeById\"(@serviceTypeId, @description, @isDeleted)";
+    }
+}
