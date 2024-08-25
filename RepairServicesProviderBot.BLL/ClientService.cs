da06c23dafd9d62b69c035526248b98d05b27971
Nikolay <tentotennot@mail.ru>
2024-08-26 00:07:28 +0300

    Add ClientService
    +AddClient +FindClient +HideClient

--- RepairServicesProviderBot.BLL/ClientService.cs
+++ RepairServicesProviderBot.BLL/ClientService.cs
@@ -1,13 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using RepairServicesProviderBot.BLL.Mappings;
+using RepairServicesProviderBot.Core.DTOs;
+using RepairServicesProviderBot.Core.InputModels;
+using RepairServicesProviderBot.Core.OutputModels;
+using RepairServicesProviderBot.DAL;
 
 namespace RepairServicesProviderBot.BLL
 {
     public class ClientService
     {
+        public UserRepository ClientRepository { get; set; }
+
+        private Mapper _mapper;
+
+        public ClientService()
+        {
+            ClientRepository = new UserRepository();
+
+            var config = new MapperConfiguration(
+                cfg => {
+                    cfg.AddProfile(new ClientMapperProfile());
+                });
+            _mapper = new Mapper(config);
+        }
+
+        public int AddClient(UserInputModel client)
+        {
+            var clientDTO = _mapper.Map<UserDTO>(client);
+
+            var clientId = ClientRepository.AddUser(clientDTO);
+
+            return clientId;
+        }
+
+        public ClientOutputModel GetClientById(int clientId)
+        {
+            var clientDTO = ClientRepository.GetUserById(clientId);
+                
+            var client = _mapper.Map<ClientOutputModel>(clientDTO);
+
+            return client;
+        }
+
+        //public ClientWithOrdersOutputModel GetClientWithOrdersById(int clientId)
+        //{
+        //    var clientDTO = ClientRepository.GetUserById(clientId);
+
+        //    var client = _mapper.Map<ClientWithOrdersOutputModel>(clientDTO);
+        //    var orders = 
+        //    client.Orders
+
+        //    return client;
+        //}
+
+        public void HideClientById(int clientId)
+        {
+            ClientRepository.HideUserById(clientId);
+        }
 
     }
 }
