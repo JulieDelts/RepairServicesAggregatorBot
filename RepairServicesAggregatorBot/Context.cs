09c2b845096c37f8da6ef9ebebec2b0c61135299
Nikolay <tentotennot@mail.ru>
2024-08-31 19:54:11 +0300

    lil fixes before start working

--- RepairServicesAggregatorBot/Context.cs
+++ RepairServicesAggregatorBot/Context.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RepairServicesAggregatorBot.Bot.States;
+using Telegram.Bot.Types;
+using Telegram.Bot;
 
 namespace RepairServicesAggregatorBot.Bot
 {
     public class Context
     {
+        public int Id { get; set; }
+
+        public long ChatId { get; set; }
+
+        public AbstractState State { get; set; }
+
+        public void HandleMessage(Update update)
+        {
+            State.HandleMessage(this, update);
+        }
+
+        public void ReactInBot(ITelegramBotClient botClient)
+        {
+            State.ReactInBot(this, botClient);
+        }
     }
 }
---
+++ RepairServicesAggregatorBot/States/OrderStates/CreatingOrderStates/AddingDescriptionOrderState.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace RepairServicesAggregatorBot.Bot.States.OrderStates.CreatingOrderStates
+{
+    public class AddingDescriptionOrderState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            if (context)
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            
+        }
+    }
+}
