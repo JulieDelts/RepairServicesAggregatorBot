c7fe0f8dd2e24962606f0a8fd92cd2561581f06c
JulieDelts <juliadelcova@gmail.com>
2024-09-04 22:14:21 +0300

    Prikoliucha

--- RepairServicesAggregatorBot/States/ClientStates/ClientMenuState.cs
+++ RepairServicesAggregatorBot/States/ClientStates/ClientMenuState.cs
@@ -28,6 +28,10 @@ namespace RepairServicesAggregatorBot.Bot.States.ClientStates
 
         public override async void ReactInBot(Context context, ITelegramBotClient botClient)
         {
+            var msg = await botClient.SendPhotoAsync(new ChatId(context.ChatId), InputFile.FromUri(new Uri(@"https://www.allrecipes.com/thmb/pENwa46VDqWGvYkycgTqHxr4xfo=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg")));
+
+            var qwe = msg.Photo.Last().FileId; //Возможно, это успех
+
             var message = await botClient.SendStickerAsync(new ChatId(context.ChatId), InputFile.FromFileId("CAACAgIAAxkBAAEIQAJm2KQmQ9lgITGWr0VCxCV2EKpFpgACDlkAAmm6yEpSDNwNc75gtzYE"));
 
             InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(
