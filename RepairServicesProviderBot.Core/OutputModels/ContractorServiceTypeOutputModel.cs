5654e321ee98a03043ea632879527d3c96d070d2
JulieDelts <juliadelcova@gmail.com>
2024-09-09 17:47:35 +0300

    Add new contractor states

--- RepairServicesProviderBot.Core/OutputModels/ContractorServiceTypeOutputModel.cs
+++ RepairServicesProviderBot.Core/OutputModels/ContractorServiceTypeOutputModel.cs
@@ -8,6 +8,8 @@ namespace RepairServicesProviderBot.Core.OutputModels
 {
     public class ContractorServiceTypeOutputModel
     {
+        public int Id { get; set; }
+
         public string ServiceTypeDescription { get; set; }
 
         public int Cost { get; set; }
