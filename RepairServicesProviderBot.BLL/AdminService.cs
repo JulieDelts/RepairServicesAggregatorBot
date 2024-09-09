3647f3a4973972946bc944205f64847f77d3965d
JulieDelts <juliadelcova@gmail.com>
2024-09-09 12:04:46 +0300

    Add admin DAL and BLL

---
+++ RepairServicesProviderBot.BLL/AdminService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using RepairServicesProviderBot.BLL.Mappings;
+using RepairServicesProviderBot.Core.OutputModels;
+using RepairServicesProviderBot.DAL;
+
+namespace RepairServicesProviderBot.BLL
+{
+    public class AdminService
+    {
+        public UserRepository UserRepository { get; set; }
+
+        private Mapper _mapper;
+
+        public AdminService()
+        {
+            UserRepository = new UserRepository();
+
+            var config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.AddProfile(new AdminMapperProfile());
+                });
+            _mapper = new Mapper(config);
+        }
+
+        public List<AdminOutputModel> GeAllAdmins()
+        {
+            var adminDTOs = UserRepository.GetAllAdmins();
+
+            List<AdminOutputModel> admins = new List<AdminOutputModel>();
+
+            foreach (var adminDTO in adminDTOs)
+            {
+                var admin = _mapper.Map<AdminOutputModel>(adminDTO);
+                admins.Add(admin);
+            }
+
+            return admins;
+        }
+    }
+}
---
+++ RepairServicesProviderBot.BLL/Mappings/AdminMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using RepairServicesProviderBot.Core.DTOs;
+using RepairServicesProviderBot.Core.InputModels;
+using RepairServicesProviderBot.Core.OutputModels;
+
+namespace RepairServicesProviderBot.BLL.Mappings
+{
+    public class AdminMapperProfile:Profile
+    {
+        public AdminMapperProfile()
+        {
+            CreateMap<UserInputModel, UserDTO>();
+            CreateMap<UserDTO, AdminOutputModel>();
+        }
+    }
+}
